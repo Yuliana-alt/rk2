69cc76731b0fa34ff007e0ebf3032f3c1fa33799
Yuliana-alt <56249228+Yuliana-alt@users.noreply.github.com>
2019-11-11 11:18:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 
+<html>
+	<head>
+		<title>rk2</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<h1>Nevzorova IU4-13B variant3</h1>
+	<form action="" method="post">
+	<p>введите число <input type="text" name="ch"</p>
+	<input type="radio name="sp" value="возвести в квадрат"<br>
+	<input type="radio name="sp" value="возвести в куб"
+	<input type="radio name="sp" value="извлечь корень"
+	<input type="radio nam
+	<?php
+	echo "task1","<br>";
+	for ($i=1;$i<=10;$i++){
+	for ($j=1;$j<=10;$j++){
+	if ($j==10){
+		echo $i*$j,"<br>";
+	}
+	else
+	echo $i*$j,"";
+	}}}
+	echo "task2","<br>";
+		$t=POST
